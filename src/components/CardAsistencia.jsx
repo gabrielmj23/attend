d03240ccd3973a6ffac4c2c8ce77eaf2c2720795
Roberth José Graffe Medina <107155833+rgraffe@users.noreply.github.com>
2023-10-15 00:08:12 -0400

    CardAsistencia
    <CardAsistencia
            id="2"
            fecha={new Date()}
            contenido="Matrices"
            color="amarillo"
          />
    
    para probarlo

---
+++ src/components/CardAsistencia.jsx
@@ -0,0 +1,63 @@
+import { PropTypes } from "prop-types";
+import { BORDES } from "../constants/colores";
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
+import InventoryOutlinedIcon from "@mui/icons-material/InventoryOutlined";
+import Boton from "./Boton";
+
+function CardAsistencia({ fecha, contenido, color }) {
+  let classNames =
+    "flex flex-row justify-center gap-4 rounded-xl mx-6 py-3 px-2 border-4 shadow-lg " +
+    BORDES[color];
+
+  let fechaActual = new Date();
+  const iconos = [<AccessTimeIcon />, <InventoryOutlinedIcon />];
+  let boton = null;
+  if (
+    fecha.getDate() === fechaActual.getDate() &&
+    fecha.getMonth() === fechaActual.getMonth() &&
+    fecha.getYear() === fechaActual.getYear()
+  ) {
+    boton = (
+      <Boton
+        texto="Tomar asistencia"
+        icono={iconos[1]}
+        tipo="primario"
+        color={color}
+      />
+    );
+  } else if (fechaActual > fecha) {
+    boton = (
+      <Boton
+        texto="Ver asistencia"
+        icono={iconos[0]}
+        tipo="secundario"
+        color={color}
+      />
+    );
+  }
+
+  return (
+    <div className={classNames}>
+      <div>
+        <p className="text-lg font-semibold">
+          {fecha.getDate() +
+            "/" +
+            (fecha.getMonth() + 1) +
+            "/" +
+            (fecha.getYear() - 100)}
+        </p>
+        <p className="text-sm font-semibold">{contenido}</p>
+        <div style={{ height: "10px" }}></div>
+        <div className="flex flex-row justify-center">{boton}</div>
+      </div>
+    </div>
+  );
+}
+
+CardAsistencia.propTypes = {
+  fecha: PropTypes.instanceOf(Date).isRequired,
+  contenido: PropTypes.string.isRequired,
+  color: PropTypes.oneOf(["amarillo", "azul", "verde", "gris"]).isRequired,
+};
+
+export default CardAsistencia;
