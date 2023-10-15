0b95ecd471b2b17f4bd7354ea46f2c8a60443400
José Andrés Bertorelli <107157202+Mostroso2003@users.noreply.github.com>
2023-10-15 15:52:44 -0400

    Etiqueta con icono y color correcto Etiqueta.jsx

--- src/components/Etiqueta.jsx
+++ src/components/Etiqueta.jsx
@@ -1,23 +1,40 @@
 import PropTypes from "prop-types";
 
-function Etiqueta({ texto, texto2}) {
-    let classNames = "flex flex-row gap-2 rounded-md p-2";
+function Etiqueta({ texto, texto2, icono}) {
+    let classNames = 
+        "flex flex-row gap-2 rounded-md p-2";
+
     const estiloInput = {
-        backgroundColor: 'lightgray', // Establecer el color del fondo de la etiqueta
-        color: 'gray', // Establecer el color de texto
-        border: '2px solid transparent',  
+        flex: 1,
+        color: 'black',
+        border: 'none',
+        backgroundColor: 'transparent',
         outline: 'none',
     };
+
+    const estiloEtiquetaContenedor = {
+        backgroundColor: '#EAF2F8',
+        color: 'gray',
+        border: '2px solid transparent',
+        display: 'flex',
+        alignItems: 'center',
+    };
+
     return (
         <label>
-            {texto} <input className={classNames} type="text" style={estiloInput} placeholder={texto2} />
+            {texto} 
+            <div className={classNames} style={estiloEtiquetaContenedor}>
+                <input type="text" style={estiloInput} placeholder={texto2} />
+                {icono}
+            </div>
         </label>
     );
 }
 
 Etiqueta.propTypes = {
     texto: PropTypes.string.isRequired,
-    texto2: PropTypes.string.isRequired,    
+    texto2: PropTypes.string.isRequired,
+    icono: PropTypes.element.isRequired,    
 };
 
 export default Etiqueta;
