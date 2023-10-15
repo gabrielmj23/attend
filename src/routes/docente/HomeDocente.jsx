ffa1663e1764199e2f5b46d6d5e20193bf3fe9cf
gabrielmj23 <gabrielmj2312@gmail.com>
2023-10-15 10:05:22 -0400

    Navegacion de aplicacion

--- src/routes/docente/HomeDocente.jsx
+++ src/routes/docente/HomeDocente.jsx
@@ -1,7 +1,12 @@
 import AppHeader from "../../components/AppHeader";
+import AppNav from "../../components/AppNav";
 import CardClase from "../../components/CardClase";
+import NavElem from "../../components/NavElem";
+import SchoolOutlinedIcon from "@mui/icons-material/SchoolOutlined";
+import AddCircleOutlineOutlinedIcon from "@mui/icons-material/AddCircleOutlineOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
 
-function HomeDocente() { 
+function HomeDocente() {
   return (
     <div className="flex flex-col gap-4">
       <AppHeader titulo="Tus Clases" color="amarillo" />
@@ -22,6 +27,29 @@ function HomeDocente() {
         nombre="Algebra Lineal"
         horario={["Martes: 7:00 am a 10:00 am", "Viernes: 12:00 pm a 2:00 pm"]}
         color="verde"
+      />
+      <AppNav
+        elementos={[
+          <NavElem
+            key={1}
+            icono={<SchoolOutlinedIcon />}
+            label="Clases"
+            ruta="/docente"
+            activo={true}
+          />,
+          <NavElem
+            key={2}
+            icono={<AddCircleOutlineOutlinedIcon />}
+            label="Nueva Clase"
+            ruta="/docente/clases/nueva"
+          />,
+          <NavElem
+            key={3}
+            icono={<SettingsOutlinedIcon />}
+            label="Ajustes"
+            ruta="/docente/ajustes"
+          />,
+        ]}
       />
     </div>
   );
