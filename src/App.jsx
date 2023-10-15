f3a34c783a08bc6f6f17f1bcfd5f3ad91fa64e3c
José Andrés Bertorelli <107157202+Mostroso2003@users.noreply.github.com>
2023-10-14 21:56:00 -0400

    Inputs en App.jsx
    Mostrar inputs para testear

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,27 @@
-import Boton from "./components/Boton";
-import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import Etiqueta from "./components/Etiqueta";
 
 function App() {
   return (
     <div className="p-3">
-      <Boton
-        texto="Hola mundo"
-        icono={<ArrowForwardIcon />}
-        color="amarillo"
-        sombra={true}
-        onClick={() => "Hola mundo"}
+      <Etiqueta
+        texto="Nombre del periodo"
+        texto2="Nombre del periodo"
+      />
+      <Etiqueta
+        texto="Fecha de inicio"
+        texto2="Fecha de inicio"
+      />
+      <Etiqueta
+        texto="Fecha de finalización"
+        texto2="Fecha de finalización"
+      />
+      <Etiqueta
+        texto="Duración(semanas)"
+        texto2="Duración(semanas)"
       />
     </div>
   );
 }
 
 export default App;
+
