ce4f214898e822e01f28941fc9b7d44dba99c50f
Roberth José Graffe Medina <107155833+rgraffe@users.noreply.github.com>
2023-10-16 20:33:08 -0400

    Card Materia Web

---
+++ src/components/CardMateriaWeb.jsx
@@ -0,0 +1,43 @@
+import PropTypes from "prop-types";
+import { BORDES } from "../constants/colores";
+import Boton from "./Boton";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import { Link } from "react-router-dom";
+
+function CardClase({ id, nombreMateria, nombreDocente, seccion, color }) {
+  const classNames =
+    "text-center flex flex-col gap-4 rounded-xl mx-6 py-3 px-2 border-4 shadow-lg " +
+    BORDES[color];
+  return (
+    <div className={classNames}>
+      <div>
+        <p className=" text-lg font-semibold">{nombreMateria}</p>
+      </div>
+      <div>
+        <p className="text-sm text-zinc-600">Docente: {nombreDocente}</p>
+        <p className="text-sm text-zinc-600">Seccion: {seccion}</p>
+      </div>
+      <div className="flex flex-row justify-center">
+        <Link to={`/docente/clases/${id}`}>
+          <Boton
+            texto="Ver clase"
+            icono={<ArrowForwardIcon />}
+            tipo="primario"
+            color={color}
+          />
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+CardClase.propTypes = {
+  id: PropTypes.string.isRequired,
+  nombreMateria: PropTypes.string.isRequired,
+  nombreDocente: PropTypes.string.isRequired,
+  seccion: PropTypes.string.isRequired,
+  horario: PropTypes.arrayOf(PropTypes.string).isRequired,
+  color: PropTypes.oneOf(["amarillo", "azul", "verde", "gris"]),
+};
+
+export default CardClase;
