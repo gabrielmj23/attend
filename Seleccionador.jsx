b0150045151c05cd3f0ff0673d0e1a5dd1254593
Roberth José Graffe Medina <107155833+rgraffe@users.noreply.github.com>
2023-11-11 20:37:50 -0400

    Add files via upload

---
+++ Seleccionador.jsx
@@ -0,0 +1,34 @@
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
+import CardAsistencia from "./CardAsistencia";
+import Barras from "./Barras";
+
+const date = new Date();
+const tabClass =
+  "bg-white-200 mb-[-1px] cursor-pointer rounded px-5 py-2 text-lg hover:bg-gray-200";
+function Seleccionador() {
+  return (
+    <div className="mx-auto w-1/2 pt-2">
+      <Tabs
+        selectedTabClassName="mb-[-1px] cursor-pointer rounded bg-green-400 px-5 py-2 text-lg"
+        className="rounded-xl border-4 border-green-500 bg-white shadow-xl "
+      >
+        <TabList className="flex list-none justify-around p-0 pb-2 pt-2">
+          <Tab className={tabClass}>Total</Tab>
+        </TabList>
+
+        <TabPanel>
+          <Barras />
+        </TabPanel>
+        <TabPanel>
+          <Barras />
+        </TabPanel>
+        <TabPanel>
+          <Barras />
+        </TabPanel>
+      </Tabs>
+    </div>
+  );
+}
+
+export default Seleccionador;
